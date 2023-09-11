da6e598fed06c40f8d115f2e4642d192a0435232
tiffanyatkinson <143667574+tiffanyatkinson@users.noreply.github.com>
2023-09-10 22:23:21 -0400

    Create Date.java

---
+++ Date.java
@@ -0,0 +1,26 @@
+public class Date {
+    public static void main(String[] args) {
+String day= "Sunday";
+int date= 10;
+String month= "September";
+int year= 2023;
+
+    System.out.print("American Format: ");
+    System.out.print(day);
+    System.out.print(", ");
+    System.out.print(month );
+    System.out.print(" ");
+    System.out.print(date);
+    System.out.print(", ");
+    System.out.println(year);
+
+    System.out.print("European Format: ");
+    System.out.print(day);
+    System.out.print(" ");
+    System.out.print(date );
+    System.out.print(" ");
+    System.out.print(month);
+    System.out.print(" ");
+    System.out.println(year);
+    }
+}
