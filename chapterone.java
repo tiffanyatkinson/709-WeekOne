9c83f33577ffb8c7d55dfc00751755e4855006ab
tiffanyatkinson <143667574+tiffanyatkinson@users.noreply.github.com>
2023-09-10 22:23:18 -0400

    Create chapterone.java

---
+++ chapterone.java
@@ -0,0 +1,7 @@
+public class chapterone {
+    public static void main(String[] args) {
+        System.out.println ("Hello, World!");  // first line
+        System.out.println("How are you?");   // another line
+        System.out.println("Third Line.\nNow a Fourth Line\n");
+    }
+}
